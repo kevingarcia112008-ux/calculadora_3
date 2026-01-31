23c596eb0e90578a73923210ffa6007a4514c8c9
Pc10 <Pc10@CPU10>
2026-01-31 10:40:21 -0600

    calculadora_3

---
+++ src/calculadora_3/Calculadora_3.java
@@ -0,0 +1,29 @@
+
+package calculadora_3;
+
+import java.util.Scanner;
+
+public class Calculadora_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+               Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = sc.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = sc.nextInt();
+
+      /*  Suma suma = new Suma();
+        Resta resta = new Resta();
+        
+        System.out.println("Suma: " + suma.calcular(a, b));
+        System.out.println("Resta: " + resta.calcular(a, b));*/
+       
+
+    }
+    
+}
